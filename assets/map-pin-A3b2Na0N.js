929524de6fb8413e08c9ab348ae105719379c729
Asiima Crisen <asiimac3@gmail.com>
2025-09-12 10:59:51 +0300

    Updates

---
+++ assets/map-pin-A3b2Na0N.js
@@ -0,0 +1,12 @@
+import{c}from"./createLucideIcon-BSJiOQxm.js";/**
+ * @license lucide-react v0.460.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=c("CirclePlay",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["polygon",{points:"10 8 16 12 10 16 10 8",key:"1cimsy"}]]);/**
+ * @license lucide-react v0.460.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=c("MapPin",[["path",{d:"M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0",key:"1r0f0z"}],["circle",{cx:"12",cy:"10",r:"3",key:"ilqhr7"}]]);export{e as C,r as M};
+//# sourceMappingURL=map-pin-A3b2Na0N.js.map
